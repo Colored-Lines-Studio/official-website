16a6380a12a2992abfbed9ef969e948547f5b28a
Chinedu Daniel <chinedudaniel7@gmail.com>
2017-04-18 08:13:23 +0100

    Start implementation of hire-us page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def hire_us
+  end
+
+  def mail_us
+    # TODO: receive form params and mail to email address
+  end
+
+  private
+
+  def mail_us_params
+    #code
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get '/hire_us' => 'pages#hire_us'
+  post '/mail_us' => 'pages#mail_us'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
