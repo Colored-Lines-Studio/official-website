6f5491a22d4189869334b52ab5b97f60f92d361f
Chinedu Daniel <chinedudaniel7@gmail.com>
2017-04-21 18:08:33 +0100

    Fix turbolinks bug

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,15 +1,17 @@
-(function () {
-  $(function () {
-    var smoothScrollTrigger = document.querySelector('[data-smooth-scroll]'),
-        smoothScrollAnchor = document.querySelector('#the-why');
+$(function () {
+  new WOW({mobile: false}).init();
+});
 
+$(document).on('turbolinks:load', function () {
+  var smoothScrollTrigger = document.querySelector('[data-smooth-scroll]'),
+      smoothScrollAnchor = document.querySelector('#the-why');
+
+  if (smoothScrollTrigger) {
     smoothScrollTrigger.addEventListener('click', function (e) {
       e.preventDefault();
       smoothScroll.animateScroll(smoothScrollAnchor, this, {
         speed: 1500
       });
     });
-
-    new WOW({mobile: false}).init();
-  });
-}());
+  }
+});
