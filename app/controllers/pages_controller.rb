8ba091b54d5724c54112f8e640f275f86793aa8f
Chinedu Daniel <chinedudaniel7@gmail.com>
2017-04-29 06:03:36 +0100

    Configure production environment for sending outbound emails

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,13 +6,16 @@ class PagesController < ApplicationController
   end
 
   def mail_us
-    @inquiry = {
-      name: "#{mail_us_params[:name]}",
-      email: "#{mail_us_params[:email]}",
-      budget: "#{mail_us_params[:budget]}",
-      short_intro: "#{mail_us_params[:type_of_work]}"
+    inquiry = {
+      name: mail_us_params[:name],
+      email: mail_us_params[:email],
+      budget: mail_us_params[:budget],
+      type_of_work: mail_us_params[:type_of_work].join(', '),
+      short_intro: mail_us_params[:short_intro],
     }
-    ContactUsMailer.thank_you(@inquiry).deliver_now
+
+    ContactUsMailer.thank_you(inquiry).deliver_now
+    ContactUsMailer.enquiry_notification(inquiry).deliver_now
 
     redirect_to root_path
   end
--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -1,11 +1,23 @@
 class ContactUsMailer < ApplicationMailer
-
   default from: 'contact@coloredlines.studio'
-  
+
   def thank_you(info)
-   @name = info[:name]
-   @email  = info[:email]
-   @budget = info[:budget]
-   mail(to: @email, subject: 'Thank You')
- end
+    mailer_variables(info)
+    mail(to: @email, subject: 'Thank You')
+  end
+
+  def enquiry_notification(info)
+    mailer_variables(info)
+    mail to: 'chinedu.daniel@andela.com', subject: "Enquiry from #{@name}"
+  end
+
+  private
+
+  def mailer_variables(info)
+    @name ||= info[:name]
+    @email  ||= info[:email]
+    @budget ||= info[:budget]
+    @type_of_work ||= info[:type_of_work]
+    @short_intro ||= info[:short_intro]
+  end
 end
