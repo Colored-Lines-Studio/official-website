ee65c7e55c9d0118d5534b1d472eab31da25ca0e
Chinedu Daniel <chinedudaniel7@gmail.com>
2017-05-14 16:36:15 +0100

    Add validation for type of work input checkboxes. Fix responsive issue on hire_us page. Restrict access of thank you page to when the hire us form is interacted with

--- app/assets/javascripts/submit_form.js
+++ app/assets/javascripts/submit_form.js
@@ -5,6 +5,10 @@
     return $('.input-field');
   };
 
+  App.typeOfWorkElem = function () {
+    return $('[name*="mail_us[type_of_work]"]');
+  };
+
   App.handleInputFieldClick = function () {
     if ( $(this).data('input-type') === 'radio' ) {
       $('[data-input-type="radio"]').removeClass('checked');
@@ -13,12 +17,31 @@
     $(this).toggleClass('checked').siblings('input').trigger('click');
   };
 
+  App.ensureTypeOfWorkIsSelected = function (e) {
+    var typeOfWorkElem = document.querySelector('[name*="mail_us[type_of_work]"]'),
+        selectedTypes = Array.prototype.slice.call($('[name*="mail_us[type_of_work]"]'))
+                              .filter(function (elem) {
+                                return $(elem).prop('checked');
+                              }).length;
+
+    if (!typeOfWorkElem) return false;
+
+    if (selectedTypes < 1) {
+      typeOfWorkElem.setCustomValidity('Please select a type of work');
+    } else {
+      typeOfWorkElem.setCustomValidity('');
+    }
+  };
+
   App.attachEvents = function () {
     App.inputFields().on('click', App.handleInputFieldClick);
+    App.typeOfWorkElem().on('change', App.ensureTypeOfWorkIsSelected);
   };
 
   App.init = function () {
     App.attachEvents();
+    // This is to add html 5 'required' behaviour to the checkboxes (multiple checkboxes)
+    App.ensureTypeOfWorkIsSelected();
   };
 
   $(document).on('turbolinks:load', App.init);
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
-  before_action
+  before_action :restrict_guest_access, only: :thank_you
+
   def home
   end
 
@@ -12,25 +13,28 @@ class PagesController < ApplicationController
       email: mail_us_params[:email],
       budget: mail_us_params[:budget],
       type_of_work: mail_us_params[:type_of_work].join(', '),
-      short_intro: mail_us_params[:short_intro],
+      short_intro: mail_us_params[:short_intro]
     }
 
     ContactUsMailer.thank_you(inquiry).deliver_now
     ContactUsMailer.enquiry_notification(inquiry).deliver_now
 
-    redirect_to thank_you_path
+    session[:thank_you_token] = SecureRandom.uuid
 
+    redirect_to thank_you_path
   end
 
   def thank_you
-
+    session.delete(:thank_you_token)
   end
-
-
 
   private
 
   def mail_us_params
     params.require(:mail_us).permit(:name, :email, :budget, :short_intro, :type_of_work => [])
+  end
+
+  def restrict_guest_access
+    redirect_to root_path unless session[:thank_you_token].present?
   end
 end
